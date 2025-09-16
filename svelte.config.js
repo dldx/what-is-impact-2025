2cdd9bcd55c85402aebc2ec920ddbcf1a29c38f2
Durand D'souza <durand1@gmail.com>
2025-09-16 15:02:38 +0100

    Added prerender configuration to ignore missing IDs in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,9 @@ const config = {
 		runes: true
 	},
 	kit: {
+		prerender: {
+			handleMissingId: 'ignore'
+		},
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
@@ -15,7 +18,7 @@ const config = {
 			assets: 'build',
 			fallback: undefined,
 			precompress: false,
-			strict: true
+			strict: true,
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
